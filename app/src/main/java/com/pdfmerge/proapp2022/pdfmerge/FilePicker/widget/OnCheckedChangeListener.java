6345468a3acabe436e357b5ad4e8d74e920e1149
inductor42 <meanshul42@gmail.com>
2021-10-26 12:38:32 +0530

    UPDATES

---
+++ app/src/main/java/com/pdfmerge/proapp2022/pdfmerge/FilePicker/widget/OnCheckedChangeListener.java
@@ -0,0 +1,8 @@
+package com.pdfmerge.proapp2022.pdfmerge.FilePicker.widget;
+
+/**
+ * @author akshay sunil masram
+ */
+public interface OnCheckedChangeListener {
+    void onCheckedChanged(MaterialCheckbox checkbox, boolean isChecked);
+}
